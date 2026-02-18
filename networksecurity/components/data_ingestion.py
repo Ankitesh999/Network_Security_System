ba5db604ce0728d2ca664b48dd5e86f5652622a2
ANKITESH TIWARI <138370094+Ankitesh999@users.noreply.github.com>
2026-02-18 13:19:26 +0530

    data ingestion configuration

---
+++ networksecurity/components/data_ingestion.py
@@ -0,0 +1,6 @@
+import os
+import sys
+
+from networksecurity.logging.logger import logger
+from networksecurity.exception import exception
+
---
+++ networksecurity/entity/config_entity.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+import os
+from networksecurity.constants.training_pipeline import *
+
+class TrainingPipelineConfig:
+    def __init__(self, time_stamp=datetime.now().strftime("%Y%m%d%H%M%S")):
+        self.pipeline_name = PIPELINE_NAME
+        self.artifact_name = ARTIFACT_DIR
+        self.artifact_dir = os.path.join(ARTIFACT_DIR, self.pipeline_name, time_stamp)
+        self.time_stamp: str = time_stamp
+
+class DataIngestionConfig:
+    def __init__(self, TrainingPipelineConfig: TrainingPipelineConfig):
+        self.data_injestion_dir = os.path.join(TrainingPipelineConfig.artifact_dir, DATA_INGESTION_DIR_NAME)
+        self.feature_store_file_path = os.path.join(self.data_injestion_dir, DATA_INGESTION_FEATURE_STORE_DIR, FILE_NAME)
+        self.training_file_path = os.path.join(self.data_injestion_dir, DATA_INGESTION_INGESTED_DIR, TRAIN_FILE_NAME)
+        self.testing_file_path = os.path.join(self.data_injestion_dir, DATA_INGESTION_INGESTED_DIR, TEST_FILE_NAME)
+        self.train_test_split_ratio: float = DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
+        self.collection_name: str = DATA_INGESTION_COLLECTION_NAME
+        self.database_name: str = DATA_INGESTION_DATABASE_NAME
+    
+
+
