ca28eef500c0d05b7059f8c5f840ef435a04a361
ANKITESH TIWARI <138370094+Ankitesh999@users.noreply.github.com>
2026-02-26 16:54:58 +0530

    mlflow tracking

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -19,6 +19,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier
 
+import mlflow
+
 class ModelTrainer:
     def __init__(self, model_trainer_config: ModelTrainerConfig, data_transformation_artifact: DataTransformationArtifact):
         try:
@@ -26,6 +28,20 @@ class ModelTrainer:
             self.data_transformation_artifact = data_transformation_artifact
         except Exception as e:
             raise custom_exception(e, sys)
+
+    def track_mlflow(self, model, metric):
+        with mlflow.start_run():
+            f1_score = metric.f1_score
+            precision_score = metric.precision_score
+            recall_score = metric.recall_score
+            accuracy_score = metric.accuracy_score
+
+            mlflow.log_params({"f1_score": f1_score,
+                               "precision_score": precision_score,
+                               "recall_score": recall_score,
+                               "accuracy_score": accuracy_score})
+
+            mlflow.sklearn.log_model(model, "model")
         
     def train_model(self, x_train, y_train, x_test, y_test):
         try:
@@ -41,32 +57,32 @@ class ModelTrainer:
             params = {
                 "KNeighborsClassifier": {
                     'n_neighbors': [3, 5, 7, 9],
-                    'weights': ['uniform', 'distance'],
-                    'metric': ['minkowski']
+                    #'weights': ['uniform', 'distance'],
+                    #'metric': ['minkowski']
                 },
                 "DecisionTreeClassifier": {
-                    'criterion': ['gini', 'entropy', 'log_loss'],
-                    'splitter': ['best', 'random'],
+                    #'criterion': ['gini', 'entropy', 'log_loss'],
+                    #'splitter': ['best', 'random'],
                     'max_features': ['sqrt', 'log2']
                 },
                 "RandomForestClassifier": {
                     'criterion': ['gini', 'entropy'],
-                    'max_features': ['sqrt', 'log2', None],
-                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                    #'max_features': ['sqrt', 'log2', None],
+                    #'n_estimators': [8, 16, 32, 64, 128, 256]
                 },
                 "AdaBoostClassifier": {
                     'learning_rate': [0.1, 0.01, 0.05, 0.001],
-                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                    #'n_estimators': [8, 16, 32, 64, 128, 256]
                 },
                 "GradientBoostingClassifier": {
                     'learning_rate': [0.1, 0.01, 0.05, 0.001],
-                    'subsample': [0.6, 0.7, 0.8, 0.9, 1.0],
-                    'max_features': ['sqrt', 'log2', None],
+                    #'subsample': [0.6, 0.7, 0.8, 0.9, 1.0],
+                    #'max_features': ['sqrt', 'log2', None],
                 },
                 "logistic_regression": {
                     'C': [0.1, 1.0, 10.0],
-                    'solver': ['lbfgs', 'liblinear'],
-                    'max_iter': [100, 200]
+                    #'solver': ['lbfgs', 'liblinear'],
+                    #'max_iter': [100, 200]
                 }
             }
 
@@ -82,12 +98,12 @@ class ModelTrainer:
                 raise Exception("No best model found")
 
             y_train_pred = best_model.predict(x_train)
-
             classification_train_metric = get_classification_metric(y_true=y_train, y_pred=y_train_pred)
+            self.track_mlflow(model=best_model, metric=classification_train_metric)
 
             y_test_pred = best_model.predict(x_test)
-
             classification_test_metric = get_classification_metric(y_true=y_test, y_pred=y_test_pred)
+            self.track_mlflow(model=best_model, metric=classification_test_metric)
 
             preprocessor = load_object(self.data_transformation_artifact.transformed_object_file_path)
 
